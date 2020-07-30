22766e0ed60b854e4e557ecc1d77fd86233ffcb5
matheusbez <matheus_isidoro@hotmail.com>
2020-07-30 15:55:02 -0300

    tarefa-integracao

---
+++ hommy-app/src/app/tab1/tab1.page.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../services/search.service';
+
+// class Republica {
+
+//   titulo: string;
+//   subtitulo: string;
+//   imagem: string;
+
+// }
+
+// class Republica2 {
+
+//   titulo: string;
+//   subtitulo: string;
+//   imagem: string;
+
+// }
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+
+
+  public republicasArray = [];
+  // republicas: Republica[];
+  // republicas2: Republica2[];
+
+  constructor(public searchService: SearchService) { }
+
+  ngOnInit() {
+
+    this.getRepublicas();
+  }
+
+  getRepublicas(){
+    this.searchService.getListRepublics().subscribe((res)=>{
+      this.republicasArray = res;
+    });
+  }
+    
+    
+    // this.republicas = [
+      // {
+      //   titulo: "República Estudantil, UFRJ-Fundão",
+      //   subtitulo: "Rio de Janeiro",
+      //   imagem: "../assets/republica.jpg",
+      // },
+      // {
+      //   titulo: "República Estudantil, UERJ",
+      //   subtitulo: "Rio de Janeiro",
+      //   imagem: "../assets/casa1.jpg",
+      // },
+      // {
+      //   titulo: "República Estudantil, UFF-Niterói",
+      //   subtitulo: "Rio de Janeiro",
+      //   imagem: "../assets/casa2.jpg",
+      // },
+      // {
+      //   titulo: "República Estudantil, UFRJ-Fundão",
+      //   subtitulo: "Rio de Janeiro",
+      //   imagem: "../assets/casa3.jpg",
+      // }
+    // ];
+
+    // this.republicas2 = [
+    //   {
+    //     titulo: "República Estudantil, UFRJ-Fundão",
+    //     subtitulo: "Rio de Janeiro",
+    //     imagem: "../assets/casa4.jpg",
+    //   },
+    //   {
+    //     titulo: "República Estudantil, UERJ",
+    //     subtitulo: "Rio de Janeiro",
+    //     imagem: "../assets/casa5.png",
+    //   },
+    //   {
+    //     titulo: "República Estudantil, UFF-Niterói",
+    //     subtitulo: "Rio de Janeiro",
+    //     imagem: "../assets/casa6.jpg",
+    //   },
+    //   {
+    //     titulo: "República Estudantil, UFRJ-Fundão",
+    //     subtitulo: "Rio de Janeiro",
+    //     imagem: "../assets/casa7.jpg",
+    //   }
+    // ];
+  
+}
+
